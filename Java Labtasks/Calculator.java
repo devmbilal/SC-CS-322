b2f22a3955f871cb75e0e4a104b4e62390222fe1
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-12-14 02:07:21 +0500

    calculator  gui added

--- Java Labtasks/Calculator.java
+++ Java Labtasks/Calculator.java
@@ -3,3 +3,113 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+public class Calculator {
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField displayField;
+    private JButton[] numberButtons;
+    private JButton[] operationButtons;
+    private JButton equalsButton;
+    private JButton clearButton;
+
+    private String operation;
+    private double firstNumber;
+    private double secondNumber;
+
+    public Calculator() {
+        frame = new JFrame("Simple Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(5, 4));
+
+        displayField = new JTextField();
+        displayField.setEditable(false);
+        panel.add(displayField);
+
+        numberButtons = new JButton[10];
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(new NumberButtonListener());
+            panel.add(numberButtons[i]);
+        }
+
+        operationButtons = new JButton[4];
+        String[] operations = {"+", "-", "*", "/"};
+        for (int i = 0; i < 4; i++) {
+            operationButtons[i] = new JButton(operations[i]);
+            operationButtons[i].addActionListener(new OperationButtonListener());
+            panel.add(operationButtons[i]);
+        }
+
+        equalsButton = new JButton("=");
+        equalsButton.addActionListener(new EqualsButtonListener());
+        panel.add(equalsButton);
+
+        clearButton = new JButton("C");
+        clearButton.addActionListener(new ClearButtonListener());
+        panel.add(clearButton);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    private class NumberButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String currentText = displayField.getText();
+            JButton button = (JButton) e.getSource();
+            displayField.setText(currentText + button.getText());
+        }
+    }
+
+    private class OperationButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton button = (JButton) e.getSource();
+            operation = button.getText();
+            firstNumber = Double.parseDouble(displayField.getText());
+            displayField.setText("");
+        }
+    }
+
+    private class EqualsButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            secondNumber = Double.parseDouble(displayField.getText());
+            double result = 0;
+
+            switch (operation) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0) {
+                        result = firstNumber / secondNumber;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Cannot divide by zero", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                    break;
+            }
+
+            displayField.setText(String.valueOf(result));
+        }
+    }
+
+    private class ClearButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            displayField.setText("");
+            firstNumber = 0;
+            secondNumber = 0;
+            operation = "";
+        }
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+}
